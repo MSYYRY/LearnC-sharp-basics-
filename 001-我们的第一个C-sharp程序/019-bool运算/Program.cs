b7fe097fc976f632da2703011fb98a56ce678e40
俞如亿 <162566052+MSYYRY@users.noreply.github.com>
2024-03-20 17:51:51 +0800

    Update Program.cs

--- 001-我们的第一个C-sharp程序/019-bool运算/Program.cs
+++ 001-我们的第一个C-sharp程序/019-bool运算/Program.cs
@@ -10,9 +10,14 @@ namespace _019_bool运算
     {
         static void Main(string[] args)
         {
+            //定义score是90。
             int score = 90;
+            
             bool sec = score > 50;
+            //如果score大于50，则输出true；反之则为false。
             Console.WriteLine(sec);
+            
+            //最终输出的结果为true。
             Console.ReadKey();
         }
     }
